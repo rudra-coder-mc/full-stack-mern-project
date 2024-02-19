68f0d8dee4ae894179b3f2dd3d5ebbb5c3990282
rudra-coder-mc <forcoding255@gmail.com>
2024-02-19 09:53:57 +0530

    react router changer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Product from "./Pages/Product";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 import AboutUs from "./Pages/About";
+import Cart from "./Pages/Cart";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -15,12 +17,18 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/product" element={<Product />} />
+          <Route path="/service" element={<Service />}>
+            <Route path=":ServiceId" element={<Service />} />
+          </Route>
+          <Route path="/product" element={<Product />}>
+            <Route path=":ProductId" element={<Product />} />
+          </Route>
           <Route path="/Login" element={<Login />} />
           <Route path="/Signup" element={<Signup />} />
           <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/Cart" element={<Cart />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
