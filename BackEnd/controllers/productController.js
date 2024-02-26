d10f044f3405d9dc9c584492f74320c2529f87e1
saunak19 <saunak.nxs@gmail.com>
2024-02-26 15:52:13 +0530

    add file upload functionality
    add file upload functionality

--- BackEnd/controllers/productController.js
+++ BackEnd/controllers/productController.js
@@ -3,11 +3,45 @@ const ErrorHander = require("../utility/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const ApiFeatures = require("../utility/apifetures");
 const { query } = require("express");
-
+const cloudinary = require("cloudinary");
 //Create Product --Admin
-exports.createProduct = catchAsyncErrors(async (req, res, next) => {
+// exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
+//   req.body.user = req.user.id;
+//   const product = await Product.create(req.body);
+
+//   res.status(201).json({
+//     success: true,
+//     product,
+//   });
+// });
+
+// Create Product -- Admin
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
+  let images = [];
+
+  if (typeof req.body.images === "string") {
+    images.push(req.body.images);
+  } else {
+    images = req.body.images;
+  }
+
+  const imagesLinks = [];
+
+  for (let i = 0; i < images.length; i++) {
+    const result = await cloudinary.v2.uploader.upload(images[i], {
+      folder: "products",
+    });
+
+    imagesLinks.push({
+      public_id: result.public_id,
+      url: result.secure_url,
+    });
+  }
+
+  req.body.images = imagesLinks;
   req.body.user = req.user.id;
+
   const product = await Product.create(req.body);
 
   res.status(201).json({
@@ -21,23 +55,21 @@ exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
   const productsCount = await Product.countDocuments();
 
   const apiFeature = new ApiFeatures(Product.find(), req.query)
-  .search()
-  .filter()
+    .search()
+    .filter();
   const products = await apiFeature.query;
   let filteredProductsCount = products.length;
 
   apiFeature.pagination(resultPerPage);
-  
+
   res.status(200).json({
     success: true,
     products,
     productsCount,
     resultPerPage,
-    filteredProductsCount
+    filteredProductsCount,
   });
-
 });
-
 
 //Get Single Product
 exports.getSingleProduct = async (req, res, next) => {
@@ -79,7 +111,7 @@ exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
   if (!product) {
     return next(new ErrorHander("Product not found", 404));
   }
-    await product.deleteOne();
+  await product.deleteOne();
 
   res.status(200).json({
     success: true,
@@ -87,85 +119,91 @@ exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-
 //Create New Review or update the review
-exports.createProductReview = catchAsyncErrors(async(req,res,next)=>{
-  const {rating,comment,productId} = req.body;
+exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
   const review = {
-    user:req.user._id,
-    name:req.user.name,
-    rating:Number(rating),
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
     comment,
   };
   const product = await Product.findById(productId);
-  const isReviewd = product.reviews.find((rev)=>rev.user.toString()===req.user._id.toString());
-  if(isReviewd){
-    product.reviews.forEach((rev)=>{
-      if(rev.user.toString()=== req.user._id.toString()){
-        (rev.rating = rating),(rev.comment = comment)
+  const isReviewd = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+  if (isReviewd) {
+    product.reviews.forEach((rev) => {
+      if (rev.user.toString() === req.user._id.toString()) {
+        (rev.rating = rating), (rev.comment = comment);
       }
     });
-  }
-  else{
-    product.reviews.push(review)
+  } else {
+    product.reviews.push(review);
     product.numOfReviews = product.reviews.length;
   }
 
   let avg = 0;
 
-  product.reviews.forEach((rev)=>{
-    avg+=rev.rating;
-  })
+  product.reviews.forEach((rev) => {
+    avg += rev.rating;
+  });
 
   product.ratings = avg / product.reviews.length;
 
-  await product.save({validateBeforeSave:false});
+  await product.save({ validateBeforeSave: false });
 
   res.status(200).json({
-    success:true
-  })
-})
+    success: true,
+  });
+});
 
 //Get All Reviews of a product
-exports.getProductReviews = catchAsyncErrors(async(req,res,next)=>{
+exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.query.id);
-  if(!product){
-    return next(new ErrorHander("Product not found",404));
+  if (!product) {
+    return next(new ErrorHander("Product not found", 404));
   }
   res.status(200).json({
-    success:true,
-    reviews:product.reviews,
+    success: true,
+    reviews: product.reviews,
   });
 });
 
 //Delete Review
-exports.deleteProductReviews = catchAsyncErrors(async(req,res,next)=>{
+exports.deleteProductReviews = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.query.productId);
-  if(!product){
-    return next(new ErrorHander("Product not found",404));
+  if (!product) {
+    return next(new ErrorHander("Product not found", 404));
   }
-  const reviews = product.reviews.filter((rev)=> rev._id.toString() !== req.query.id.toString());
+  const reviews = product.reviews.filter(
+    (rev) => rev._id.toString() !== req.query.id.toString()
+  );
 
   let avg = 0;
 
-  reviews.forEach((rev)=>{
-    avg+=rev.rating;
-  })
+  reviews.forEach((rev) => {
+    avg += rev.rating;
+  });
 
   const ratings = avg / reviews.length;
   const numOfReviews = reviews.length;
 
-  await product.findByIdAndUpdate(req.query.productId,{
-    reviews,
-    ratings,
-    numOfReviews,
-  },{
-    new:true,
-    runValidators:true
-  });
-  
+  await product.findByIdAndUpdate(
+    req.query.productId,
+    {
+      reviews,
+      ratings,
+      numOfReviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
   res.status(200).json({
-    success:true,
-    reviews:product.reviews,
+    success: true,
+    reviews: product.reviews,
   });
 });
