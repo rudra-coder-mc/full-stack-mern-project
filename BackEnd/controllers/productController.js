0f7fce2069cf1f21a6c79613be03419320e56022
rudra-coder-mc <forcoding255@gmail.com>
2024-03-06 09:32:45 +0530

    improvment

--- BackEnd/controllers/productController.js
+++ BackEnd/controllers/productController.js
@@ -10,7 +10,7 @@ const path = require("path");
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   let fileName = Date.now() + "-" + req.files.image.name;
   let newPath = path.join(process.cwd(), "uploads", fileName);
-  console.log(req.files.image.mv(newPath));
+  req.files.image.mv(newPath);
   req.body.user = req.user.id;
   // const product = await Product.create(req.body);
 
@@ -41,7 +41,7 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
       price: req.body.price,
       image: [
         {
-          url: newPath,
+          url: req.files.image.name,
         },
       ],
       category: req.body.category,
@@ -58,107 +58,6 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
     res.status(500).send(err.message);
   }
 });
-
-// // Set up multer storage
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "uploads/"); // Where to store uploaded files
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + "-" + file.originalname); // Rename files with unique name
-//   },
-// });
-
-// // Initialize multer upload
-
-// exports.upload = multer({ storage: storage });
-
-// // Create Product --Admin
-// exports.createProduct = catchAsyncErrors(async (req, res, next) => {
-//   console.log(req.file);
-//   console.log(req.body);
-//   // req.body.user = req.user.id;
-//   // // const product = await Product.create(req.body);
-//   // const Image = req.body.Image;
-//   if (
-//     !req.body.name ||
-//     !req.body.description ||
-//     !req.body.price ||
-//     !req.body.category ||
-//     !req.body.stock
-//   ) {
-//     return res
-//       .status(400)
-//       .json({ message: "Missing required fields in request body." });
-//   }
-
-//   // Assuming you are using Multer middleware
-//   if (!req.file) {
-//     return res
-//       .status(400)
-//       .json({ message: "Please select an image to upload." });
-//   }
-//   console.log(req.file);
-//   try {
-//     // Save image information to MongoDB
-//     const product = await Product.create({
-//       name: req.body.name,
-//       description: req.body.description,
-//       price: req.body.price,
-//       image: [
-//         {
-//           public_id: req.file.filename, // Assuming the filename is used as the public_id
-//           url: req.file.path, // Assuming the path is used as the URL
-//         },
-//       ],
-//       category: req.body.category,
-//       Stock: req.body.Stock,
-//       numOfReviews: null,
-//       reviews: [],
-//     });
-//     res.status(201).json({
-//       success: true,
-//       product,
-//     });
-//     res.send("Product uploaded successfully");
-//   } catch (err) {
-//     res.status(500).send(err.message);
-//   }
-// });
-
-// Create Product -- Admin
-// exports.createProduct = catchAsyncErrors(async (req, res, next) => {
-//   let images = [];
-
-//   if (typeof req.body.images === "string") {
-//     images.push(req.body.images);
-//   } else {
-//     images = req.body.images;
-//   }
-
-//   const imagesLinks = [];
-
-//   for (let i = 0; i < images.length; i++) {
-//     const result = await cloudinary.v2.uploader.upload(images[i], {
-//       folder: "products",
-//     });
-
-//     imagesLinks.push({
-//       public_id: result.public_id,
-//       url: result.secure_url,
-//     });
-//   }
-
-//   req.body.images = imagesLinks;
-//   req.body.user = req.user.id;
-
-//   const product = await Product.create(req.body);
-
-//   res.status(201).json({
-//     success: true,
-//     product,
-//   });
-// });
 
 exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
   const resultPerPage = 8;
